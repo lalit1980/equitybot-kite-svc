f834965128269d4a53befd12cbe779d11443a8c7
Lalit Singh <lalit1980@gmail.com>
2018-07-14 22:36:49 +0800

    test

--- src/main/java/com/equitybot/trade/order/model/AlgorithmModel.java
+++ src/main/java/com/equitybot/trade/order/model/AlgorithmModel.java
@@ -1,26 +1,28 @@
 package com.equitybot.trade.order.model;
 
 import java.util.List;
+import java.util.Map;
 
 import org.ta4j.core.Bar;
 
 public class AlgorithmModel {
 	
-	private List<Bar> buyBar;
-	private List<Bar> sellBar;
+	private Map<Long, Bar> buyBarMap;
+	private Map<Long, Bar> sellBarMap;
+	public Map<Long, Bar> getBuyBarMap() {
+		return buyBarMap;
+	}
+	public void setBuyBarMap(Map<Long, Bar> buyBarMap) {
+		this.buyBarMap = buyBarMap;
+	}
+	public Map<Long, Bar> getSellBarMap() {
+		return sellBarMap;
+	}
+	public void setSellBarMap(Map<Long, Bar> sellBarMap) {
+		this.sellBarMap = sellBarMap;
+	}
 	
-	public List<Bar> getBuyBar() {
-		return buyBar;
-	}
-	public void setBuyBar(List<Bar> buyBar) {
-		this.buyBar = buyBar;
-	}
 	
-	public List<Bar> getSellBar() {
-		return sellBar;
-	}
-	public void setSellBar(List<Bar> sellBar) {
-		this.sellBar = sellBar;
-	}
+	
 
 }
