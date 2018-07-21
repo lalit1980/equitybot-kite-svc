e85a6e33fc6505924f437f4ab1a991f923e9f228
Lalit <lalit1980@gmail.com>
2018-07-21 23:40:53 +0800

    New Changes

---
+++ src/main/java/com/equitybot/trade/service/cache/UpdateCacheService.java
@@ -0,0 +1,4 @@
+package com.equitybot.trade.service.cache;
+
+public class UpdateCacheService {
+}
---
+++ src/main/java/com/equitybot/trade/service/cache/ignite/IgniteCache.java
@@ -0,0 +1,35 @@
+package com.equitybot.trade.service.cache.ignite;
+
+import com.equitybot.trade.ignite.configs.IgniteConfig;
+import com.zerodhatech.models.Tick;
+import org.apache.ignite.configuration.CacheConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+@Service
+@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
+public class IgniteCache {
+
+    @Autowired
+    IgniteConfig igniteConfig;
+    private org.apache.ignite.IgniteCache<Long, Tick> maxTrailStopLossTickCache;
+    private org.apache.ignite.IgniteCache<Long, Tick> latestTickCache;
+
+    public IgniteCache(){
+        CacheConfiguration<Long, Tick> ccfgLatestTickParams = new CacheConfiguration<>("CachedLatestTick");
+        this.latestTickCache = igniteConfig.getInstance().getOrCreateCache(ccfgLatestTickParams);
+
+        CacheConfiguration<Long, Tick> ccfgMaxTrailStopLossTickCacheParams = new CacheConfiguration<>("MaxTrailStopLossTickCache");
+        this.maxTrailStopLossTickCache = igniteConfig.getInstance().getOrCreateCache(ccfgMaxTrailStopLossTickCacheParams);
+    }
+
+    public org.apache.ignite.IgniteCache<Long, Tick> getMaxTrailStopLossTickCache() {
+        return maxTrailStopLossTickCache;
+    }
+
+    public org.apache.ignite.IgniteCache<Long, Tick> getLatestTickCache() {
+        return latestTickCache;
+    }
+}
